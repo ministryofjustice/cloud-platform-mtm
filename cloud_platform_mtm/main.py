8db5721eb4a397ee7818551e6a2a070053ccacf9
Mike Bell <mike.bell1@digital.justice.gov.uk>
2024-04-23 13:38:05 +0100

    feat: Move migrate module function into its own file

--- cloud_platform_mtm/main.py
+++ cloud_platform_mtm/main.py
@@ -2,7 +2,7 @@ import typer
 
 from typing_extensions import Annotated
 from pathlib import Path
-from cloud_platform_mtm.utility import utility, source, destination
+from cloud_platform_mtm.utility import utility, migrateModule
 
 
 app = typer.Typer()
@@ -26,17 +26,7 @@ def migrate(
     sourceCheck = utility.checkFile(source_path, "tfstate")
 
     if destinationCheck == sourceCheck == True:
-        migrateResources(module, destination_path, source_path)
-
-def migrateResources(module: str, destinationState, sourceState):
-    moduleName = "module." + module
-    moduleState = destination.getModuleState(sourceState, moduleName)
-
-    destinationNewState = destination.mergeModuleState(destinationState, moduleState, module)
-    utility.saveState("core", destinationNewState)
-
-    sourceNewState = source.deleteModuleState(sourceState, moduleName)
-    utility.saveState("components", sourceNewState)
+        migrateModule.migrateResources(module, destination_path, source_path)
 
 if __name__ == "__main__":
     app()
